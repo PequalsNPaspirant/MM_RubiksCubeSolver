fea7d8512518acfd3d029b106f0c86a21128ad46
PequalsNPaspirant <marutimhetre@gmail.com>
2019-01-05 21:46:25 -0500

    Added next version v4 of Rubik's Cube Solver.

--- RubiksCube/RubiksCubeModel.cpp
+++ RubiksCube/RubiksCubeModel.cpp
@@ -14,11 +14,13 @@ namespace mm {
 	unique_ptr<RubiksCubeModel> createRubiksCubeModel_v1(int size);
 	unique_ptr<RubiksCubeModel> createRubiksCubeModel_v2(int size);
 	unique_ptr<RubiksCubeModel> createRubiksCubeModel_v3(int size);
+	unique_ptr<RubiksCubeModel> createRubiksCubeModel_v4(int size);
 
 	unordered_map<string, fptrRubiksCubeModelCreator> RubiksCubeModelFactory::rubiksCubeFactoryMap_ = {
 		unordered_map<string, fptrRubiksCubeModelCreator>::value_type("RubiksCubeModel_v1", createRubiksCubeModel_v1),
 		unordered_map<string, fptrRubiksCubeModelCreator>::value_type("RubiksCubeModel_v2", createRubiksCubeModel_v2),
-		unordered_map<string, fptrRubiksCubeModelCreator>::value_type("RubiksCubeModel_v3", createRubiksCubeModel_v3)
+		unordered_map<string, fptrRubiksCubeModelCreator>::value_type("RubiksCubeModel_v3", createRubiksCubeModel_v3),
+		unordered_map<string, fptrRubiksCubeModelCreator>::value_type("RubiksCubeModel_v4", createRubiksCubeModel_v4)
 	};
 
 	unique_ptr<RubiksCubeModel> RubiksCubeModelFactory::getRubiksCubeModel(const string& modelName, int size)
